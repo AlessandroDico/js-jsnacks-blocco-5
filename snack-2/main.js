56682cd06b6946c0fbb92a2c1bdda53ccb1cd77a
Ale <alessandrochb@gmail.com>
2020-11-02 16:38:04 +0100

    snack2-aggiunta funzione per mettere i numeri sempre in ordine crescente

--- snack-2/main.js
+++ snack-2/main.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 
     var rangeNumbers = [];
 
+// CHIEDO DUE NUMERI VALIDI ALL'UTENTE
     do {
         var userNumbers = parseInt(prompt('inserisci un numero da 1 a 10'));
         if (isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10) {
@@ -18,6 +19,13 @@ $(document).ready(function(){
 
     } while ((isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10 || rangeNumbers.length < 2));
 
-    console.log(rangeNumbers);
+    // console.log(rangeNumbers);
+
+    // FUNZIONE PER METTERE IN NUMERI IN UN ARRAY IN ORDINE CRESCENTE COSI DA EVITARE COSE STRANE SE L'UTENTE METTE PRIMA UN MUNERO MAGGIORE DI QUELLO DOPO
+    var order = rangeNumbers.sort(function(a, b){return a-b});
+    console.log(order);
+
+
+
 
 });
