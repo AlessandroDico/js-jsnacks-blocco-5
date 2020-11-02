2845f53a3b3aedbc8b55753ac6e1d54093615e7b
Ale <alessandrochb@gmail.com>
2020-11-02 17:36:54 +0100

    snack-3 creato un array di oggetti e creato la sua copia

--- snack-3/main.js
+++ snack-3/main.js
@@ -4,4 +4,40 @@ $(document).ready(function(){
 
     // JSnack3: A partire da un array di oggetti, creare una copia dell'array e aggiungere ai singoli elementi dell'array una nuova proprietà "position" che contiene una lettera casuale.
 
+    // creo un array di oggetti
+
+    // creo una copia dell'array primo
+
+    // aggiungo ad ogni elemento del nuovo array una proprietà es.'position' con valore una lettera casuale
+
+    var animals = [
+        {
+            'name' : 'pippo',
+            'type' : 'dog',
+            'age' : 5,
+        },
+        {
+            'name' : 'silvestro',
+            'type' : 'cat',
+            'age' : 7,
+        },
+        {
+            'name' : 'beep-beep',
+            'type' : 'roadrunner',
+            'age' : 4,
+        },
+    ];
+
+    console.log(animals);
+
+    console.log(animals[0].name);
+
+    // creo una copia dell'array
+
+    var newAnimals = animals;
+
+    console.log(newAnimals); 
+
+
+
 });
