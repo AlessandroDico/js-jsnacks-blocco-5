0d6308cb502cd675578356df1b78610ac9da36cf
Ale <alessandrochb@gmail.com>
2020-11-02 16:26:34 +0100

    snack-2-- creato array di nomi e richiesta dei due numeri all'utente

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
 
     // JSnack2: Creare un array di nomi e chiedere all'utente due numeri. Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
 
+    var names = ['pippo', 'mario', 'luca', 'pluto', 'maria', 'marco', 'paola', 'giovanna', 'piera', 'mimmo'];
+
+    var rangeNumbers = [];
+
+    do {
+        var userNumbers = parseInt(prompt('inserisci un numero da 1 a 10'));
+        if (isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10) {
+            alert('numero non valido')
+        } else {
+            rangeNumbers.push(userNumbers);
+        }
+
+    } while ((isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10 || rangeNumbers.length < 2));
+
+    console.log(rangeNumbers);
+
 });
