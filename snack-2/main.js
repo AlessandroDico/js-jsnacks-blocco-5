d37ae8a897cc210764d138af3ec43e1053f2f39d
Ale <alessandrochb@gmail.com>
2020-11-02 17:25:15 +0100

    snack-2-- fatto combaciare il conteggio "umano" con quello informatico

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     // JSnack2: Creare un array di nomi e chiedere all'utente due numeri. Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
 
-    var names = ['pippo', 'mario', 'luca', 'pluto', 'maria', 'marco', 'paola', 'giovanna', 'piera', 'mimmo', 'pino'];
+    var names = ['pippo', 'mario', 'luca', 'pluto', 'maria', 'marco', 'paola', 'giovanna', 'piera', 'mimmo'];
 
     var rangeNumbers = [];
 
@@ -12,11 +12,12 @@ $(document).ready(function(){
 
 // CHIEDO DUE NUMERI VALIDI ALL'UTENTE
     do {
-        var userNumbers = parseInt(prompt('inserisci un numero da 0 a 10'));
-        if (isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10) {
+        var userNumbers = parseInt(prompt('inserisci un numero da 1 a 10'));
+        if (isNaN(userNumbers) || userNumbers <= 0 || userNumbers > 10) {
             alert('numero non valido')
         } else {
-            rangeNumbers.push(userNumbers);
+            rangeNumbers.push(userNumbers - 1);
+            // metto userNumbers - 1 in modo da farlo combaciare con le posizioni dell'array in conteggio "umano"
         }
 
     } while ((isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10 || rangeNumbers.length < 2));
