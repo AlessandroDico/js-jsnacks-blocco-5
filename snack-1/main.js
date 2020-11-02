c578626b426a17cd625d67a4d7fcad210686a433
Ale <alessandrochb@gmail.com>
2020-11-02 14:37:03 +0100

    creato array di oggetti snack 1

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,4 +4,34 @@ $(document).ready(function(){
 
     // JSnack1: Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
+    // creare un array di oggetti
+    // quindi [{ }]
+    // inserirò all'interno più bici quindi più oggetti
+
+    var bike = [
+        {
+            'name' : 'bianchi',
+            'weight' : 10,
+        },
+        {
+            'name' : 'specialized',
+            'weight' : 9,
+        },
+        {
+            'name' : 'cannondale',
+            'weight' : 8,
+        },
+        {
+            'name' : 'colnago',
+            'weight' : 11,
+        },
+    ];
+
+    // ogni bici avrà come proprietà il nome e il peso
+    // quindi le chiavi saranno name: e weight:
+
+    // stampo a schermo la bici con il peso minore
+    // quindi devo capire quale bici ha il peso minore prima
+
+
 });
